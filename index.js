85414bfd8c24029cca16b459eb47ac4a92bf14be
Pherpher089 <Christutor089@gmail.com>
2019-08-21 18:44:22 -0700

    Created the database

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const express = require("express");
+//Import server object
+const server = require("./server.js");
+
+//Assign port variable
+const PORT = process.env.PORT || 5000;
+
+//Set the server to listen on PORT or 5000
+server.listen(PORT, () => console.log(`Magic happening on port: ${PORT}`));
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+//import express
 const express = require("express");
 
-const server = expres();
+//set up server object with express
+const server = express();
 
+//use json middle ware
 server.use(express.json());
 
+//Set default get method on the root
 server.get("/", (req, res) => {
 	res.status(200).send("<h1>Hello from auth challenge! </h1>");
 });
 
+//export server
 module.exports = server;
